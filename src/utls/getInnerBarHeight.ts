150b669714c7b6b7cbf7fabc9458c4e5c1322c9d
VovaTereschenko <t.rimidalv@gmail.com>
2021-04-11 21:07:20 +0300

    minYvalue now has a default value

--- src/utls/getInnerBarHeight.ts
+++ src/utls/getInnerBarHeight.ts
@@ -1,4 +1,4 @@
-const getInnerBarHeight = (barValue: number, maxYValue: number, minYValue: number) =>
+const getInnerBarHeight = (barValue: number, maxYValue: number, minYValue: number = 0) =>
   `${(Number(barValue) - minYValue) * 100/(maxYValue - minYValue)}%`
 
 export default getInnerBarHeight
